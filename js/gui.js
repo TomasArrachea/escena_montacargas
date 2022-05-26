cf5f929ce7411a624a610a97708395e718703ec1
tomasarrachea <tarrachea@fi.uba.ar>
2022-05-26 12:27:27 -0300

    Menu funcionando

--- js/gui.js
+++ js/gui.js
@@ -1,30 +1,25 @@
+function initMenu(escena) {
+	var app = {
+		tipoSuperficie: 'barrido',
+		forma2dBarrido: 'B1',
+		forma2dRevolucion: 'A1',
+		angulo: 0,
+		generarImpresion: function() {
+			let impresora = escena.getImpresora();
+			if (tipoSuperficie == 'barrido') {
+				impresora.generarImpresion(tipoSuperficie, forma2dBarrido, angulo/2*Math.PI);
+			} else {
+				impresora.generarImpresion(tipoSuperficie, forma2dRevolucion, angulo/2*Math.PI);
+			}
+		}		
+	};
+	var gui = new dat.GUI();
 
-
-function generarImpresion() {
-        /*
-        let impresora = objects[1];
-        if (tipoSuperficie == 'barrido') {
-                impresora.generarImpresion(tipoSuperficie, forma2dbarrido, angulo);
-        } else {
-                impresora.generarImpresion(tipoSuperficie, forma2dRevolucion, angulo);
-        }
-        */
-}
-
-
-function initMenu() {
-        var tipoSuperficie = 'barrido';
-        var forma2dBarrido = 'B1';
-        var forma2dRevolucion = 'A1';
-        var angulo = 0;
-        var generarImpresion = () => {return true};
-
-        var gui = new dat.GUI();
-        gui.add(window, "tipoSuperficie",["barrido","revolucion"]).name("Tipo de superficie");
-        gui.add(window, "forma2dRevolucion",["A1","A3","A3","A4"]).name("Forma 2D de revolución");
-        gui.add(window, "forma2dBarrido",["B1","B3","B3","B4"]).name("Forma 2D de barrido");
-        gui.add(window, "angulo",0, 360).step(1).name("Angulo");
-        gui.add(window, "generarImpresion").name("Generar");
+	gui.add(app, "tipoSuperficie",["barrido","revolucion"]);
+	gui.add(app, "forma2dRevolucion",["A1","A3","A3","A4"]);
+	gui.add(app, "forma2dBarrido",["B1","B3","B3","B4"]);
+	gui.add(app, "angulo",0, 360).step(1);
+	gui.add(app, "generarImpresion");
 };
 
 export {initMenu}
