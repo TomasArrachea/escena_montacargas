be0e62230afadd6cbbc2c0979864c5e0de9e2145
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-07 00:13:02 -0300

    fix textures paths

--- js/common/textures.js
+++ js/common/textures.js
@@ -1,6 +1,6 @@
-export const WOOD = '/maps/Wood06_1K_BaseColor.png';
-export const RUST = '/maps/texturaGrua.jpg';
-export const WALL = '/maps/CorrugatedMetalPanel02_1K_BaseColor.png';
-export const PRINTER = '/maps/greyRoom1_back.jpg';
-export const FLOOR = '/maps/StoneTilesFloor01_1K_BaseColor.png';
-export const WHEEL = '/maps/rueda.jpg';
+export const WOOD = '../maps/Wood06_1K_BaseColor.png';
+export const RUST = '../maps/texturaGrua.jpg';
+export const WALL = '../maps/CorrugatedMetalPanel02_1K_BaseColor.png';
+export const PRINTER = '../maps/greyRoom1_back.jpg';
+export const FLOOR = '../maps/StoneTilesFloor01_1K_BaseColor.png';
+export const WHEEL = '../maps/rueda.jpg';
