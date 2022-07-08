f6b6f97783c76ea12ef12662062285908efb61d1
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-08 17:36:20 -0300

    modificado posicion y tamaño de panel de impresora

--- js/objects/Cabezal.js
+++ js/objects/Cabezal.js
@@ -14,58 +14,58 @@ export class Cabezal extends Objeto3D {
         agarreBarra.setColor(RGB_GREEN);
         this.agregarHijo(agarreBarra);
 
-        var tirante = new Cubo(this, 0.1, 0.15, 1);
-        tirante.setPosicion(0.1, 0, -0.6);
+        var tirante = new Cubo(this, 0.1, 0.15, 0.7);
+        tirante.setPosicion(0.1, 0, -0.4);
         tirante.setColor(RGB_GREEN);
         this.agregarHijo(tirante);
 
-        var tirante = new Cubo(this, 0.1, 0.15, 1);
-        tirante.setPosicion(-0.1, 0, -0.6);
+        var tirante = new Cubo(this, 0.1, 0.15, 0.7);
+        tirante.setPosicion(-0.1, 0, -0.4);
         tirante.setColor(RGB_GREEN);
         this.agregarHijo(tirante);
 
         var agarrePanel = new Cubo(this, 0.6, 0.2, 0.3);
-        agarrePanel.setPosicion(0, 0, -1.2);
+        agarrePanel.setPosicion(0, 0, -0.9);
         agarrePanel.setColor(RGB_GREEN);
         this.agregarHijo(agarrePanel);
 
-        this.panel = new Cubo(this, 1, 0.05, 1);
-        this.panel.setPosicion(0, -0.05, -1.2);
+        this.panel = new Cubo(this, 1.1, 0.05, 1.1);
+        this.panel.setPosicion(0, -0.05, -0.9);
         this.panel.setColor(RGB_GREEN);
         this.agregarHijo(this.panel);
 
         var luz = new Luz(this);
-        luz.setPosicion(-0.5, -0.05, -1.7);
+        luz.setPosicion(-0.55, -0.05, -1.45);
         luz.setColor(RGB_RED);
         this.agregarHijo(luz);
 
         var luz = new Luz(this);
-        luz.setPosicion(0.5, -0.05, -1.7);
+        luz.setPosicion(0.55, -0.05, -1.45);
         luz.setColor(RGB_RED);
         this.agregarHijo(luz);
 
         var luz = new Luz(this);
-        luz.setPosicion(-0.5, -0.05, -0.7);
+        luz.setPosicion(-0.55, -0.05, -0.35);
         luz.setColor(RGB_RED);
         this.agregarHijo(luz);
 
         var luz = new Luz(this);
-        luz.setPosicion(0.5, -0.05, -0.7);
+        luz.setPosicion(0.55, -0.05, -0.35);
         luz.setColor(RGB_RED);
         this.agregarHijo(luz);
     }
 
     setLights() {
-        var posLight = this.getPosicionMundo([-0.5, -0.05, -1.7]);
+        var posLight = this.getPosicionMundo([-0.55, -0.05, -1.45]);
         this.setPointLight(1, posLight);
 
-        var posLight = this.getPosicionMundo([0.5, -0.05, -1.7]);
+        var posLight = this.getPosicionMundo([0.55, -0.05, -1.45]);
         this.setPointLight(2, posLight);
 
-        var posLight = this.getPosicionMundo([-0.5, -0.05, -0.7]);  
+        var posLight = this.getPosicionMundo([-0.55, -0.05, -0.35]);  
         this.setPointLight(3, posLight);
 
-        var posLight = this.getPosicionMundo([0.5, -0.05, -0.7]);
+        var posLight = this.getPosicionMundo([0.55, -0.05, -0.35]);
         this.setPointLight(4, posLight);
     }
 
