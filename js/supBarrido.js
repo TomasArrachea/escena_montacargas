669b3ee00572cd5274c9d1db31f3afc683e92f54
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-12 19:46:38 -0300

    fix rotacion barrido - falta corregir normales

--- js/supBarrido.js
+++ js/supBarrido.js
@@ -1,5 +1,7 @@
+import { mat4, vec3, vec4, vec2 } from 'https://cdn.skypack.dev/gl-matrix';
+
 class SupBarrido {
-    constructor(curva, altura, torsion = 0, esImpresion = false, filas = 8, columnas = 8) {
+    constructor(curva, altura, torsion = 0, esImpresion = false, filas = 20, columnas = 20) {
         this.puntos = curva.puntos;
         this.topePuntos = this.puntos.length / 2 - 1;
         this.normales = curva.normales;
@@ -9,6 +11,7 @@ class SupBarrido {
         this.esImpresion = esImpresion; // si es impresion, la base esta en el eje y = 0 y no tiene tapas
         this.filas = filas;
         this.columnas = columnas;
+        this.matrizRotacion = mat4.create();
     }
 
     getPos(u, v) {
@@ -40,33 +43,26 @@ class SupBarrido {
         }
 
         // rotacion alrededor del eje y con angulo de torsion
-        var angulo = v * this.torsion * Math.PI / 180;
-        x = Math.cos(angulo) * x - Math.sin(angulo) * z;
-        z = Math.sin(angulo) * x + Math.cos(angulo) * z;
-        return [x, y, z];
+        var angulo = v * this.torsion * (Math.PI / 180);
+        mat4.fromRotation(this.matrizRotacion, angulo, vec3.fromValues(0, 1, 0));
+        var pos = vec3.fromValues(x, y, z);
+        vec3.transformMat4(pos, pos, this.matrizRotacion);
+        
+        return [pos[0], pos[1], pos[2]];
     }
-
+    
     getNormal(u, v) {
-        if (!this.esImpresion) {
-            if (v == 0) {
-                return [0, -1, 0];
-            } else if (v == 1 / this.filas) {
-                return [0, -1, 0];
-            } else if (v == (this.filas - 1) / this.filas) {
-                return [0, 1, 0];
-            } else if (v == 1) {
-                return [0, 1, 0];
-            }
-        }
-
         var index = Math.floor(u * this.topeNormales)
         var x = this.normales[index * 2];
+        var y = 0;
         var z = this.normales[index * 2 + 1];
 
         var angulo = this.torsion * v * Math.PI / 180;
-        x = Math.cos(angulo) * x - Math.sin(angulo) * z;
-        z = Math.sin(angulo) * x + Math.cos(angulo) * z;
-        return [x, 0, z];
+        var pos = vec3.fromValues(x, y, z);
+        mat4.fromRotation(this.matrizRotacion, angulo, vec3.fromValues(0, 1, 0));
+        vec3.transformMat4(pos, pos, this.matrizRotacion);
+        
+        return [pos[0], pos[1], pos[2]];
     }
 
     getCoordenadasTextura(u, v) {
@@ -75,7 +71,7 @@ class SupBarrido {
         // } else if (v == 1 / this.filas) {
         //     return [0, -1];
         // } else if (as) {
-            
+
         // } else if (as) {
 
         // } else if (v == (this.filas - 1) / this.filas) {
