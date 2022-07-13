8eee6b0cc97c0fb9180fccdcb84c7700eabd443a
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-13 14:23:20 -0300

    updated gui

--- js/gui.js
+++ js/gui.js
@@ -11,16 +11,28 @@ function initMenu(escena) {
 			} else {
 				escena.generarImpresion(this.tipoSuperficie, this.forma2dRevolucion, this.textura  + '.png', 0);
 			}
+		},
+		verComandos: function() {
+			window.open('https://calico-transport-086.notion.site/Escena-montacargas-036b4ee633734976baa7fe22efabc107');
 		}
 	};
 	var gui = new dat.GUI();
+	var impresora = gui.addFolder('Impresora');
 
-	gui.add(app, "tipoSuperficie", ["barrido", "revolucion"]);
-	gui.add(app, "forma2dRevolucion", ["A1", "A2", "A3", "A4"]);
-	gui.add(app, "forma2dBarrido", ["B1", "B2", "B3", "B4"]);
-	gui.add(app, "angulo", 0, 360).step(1);
-	gui.add(app, "textura", ['patron1', 'patron2', 'patron3', 'patron4', 'patron5', 'patron6', 'patron7']);
-	gui.add(app, "generarImpresion");
+	impresora.add(app, "tipoSuperficie", ["barrido", "revolucion"]).name('Tipo de superficie');
+	impresora.add(app, "forma2dRevolucion", ["A1", "A2", "A3", "A4"]).name('Forma de revolución');
+	impresora.add(app, "forma2dBarrido", ["B1", "B2", "B3", "B4"]).name('Forma de barrido');
+	impresora.add(app, "angulo", 0, 360).step(1).name('Ángulo de torsión');
+	impresora.add(app, "textura", ['patron1', 'patron2', 'patron3', 'patron4', 'patron5', 'patron6', 'patron7']).name('Textura');
+	impresora.add(app, "generarImpresion").name('GENERAR IMPRESIÓN');
+	impresora.open();
+
+	var instrucciones = gui.addFolder('Instrucciones');
+	instrucciones.add(app, "verComandos").name('Ver instrucciones');
+	instrucciones.open();
+
+	gui.width = 300;
+
 };
 
 export { initMenu }
