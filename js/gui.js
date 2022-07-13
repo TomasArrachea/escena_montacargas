8361a1c588c35fd54b62080a85c3183d6a6f54ff
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-13 14:40:15 -0300

    updated gui

--- js/gui.js
+++ js/gui.js
@@ -9,22 +9,34 @@ function initMenu(escena) {
 			if (this.tipoSuperficie == 'barrido') {
 				escena.generarImpresion(this.tipoSuperficie, this.forma2dBarrido, this.textura + '.png', this.angulo);
 			} else {
-				escena.generarImpresion(this.tipoSuperficie, this.forma2dRevolucion, this.textura  + '.png', 0);
+				escena.generarImpresion(this.tipoSuperficie, this.forma2dRevolucion, this.textura + '.png', 0);
 			}
 		},
-		verComandos: function() {
+		verComandos: function () {
 			window.open('https://calico-transport-086.notion.site/Escena-montacargas-036b4ee633734976baa7fe22efabc107');
 		}
 	};
 	var gui = new dat.GUI();
 	var impresora = gui.addFolder('Impresora');
+	var controllerBarrido, controllerRev;
 
-	impresora.add(app, "tipoSuperficie", ["barrido", "revolucion"]).name('Tipo de superficie');
-	impresora.add(app, "forma2dRevolucion", ["A1", "A2", "A3", "A4"]).name('Forma de revolución');
-	impresora.add(app, "forma2dBarrido", ["B1", "B2", "B3", "B4"]).name('Forma de barrido');
-	impresora.add(app, "angulo", 0, 360).step(1).name('Ángulo de torsión');
-	impresora.add(app, "textura", ['patron1', 'patron2', 'patron3', 'patron4', 'patron5', 'patron6', 'patron7']).name('Textura');
 	impresora.add(app, "generarImpresion").name('GENERAR IMPRESIÓN');
+	impresora.add(app, "textura", ['patron1', 'patron2', 'patron3', 'patron4', 'patron5', 'patron6', 'patron7']).name('Textura');
+	impresora.add(app, "angulo", 0, 360).step(1).name('Ángulo de torsión');
+	impresora.add(app, "tipoSuperficie", ["barrido", "revolucion"]).name('Tipo de superficie').onFinishChange(function (value) {
+		if (value == 'barrido') {
+			if (controllerRev)
+				controllerRev.remove();
+			controllerBarrido = impresora.add(app, "forma2dBarrido", ["B1", "B2", "B3", "B4"]).name('Forma de barrido');
+
+		} else {
+			if (controllerBarrido)
+				controllerBarrido.remove();
+			controllerRev = impresora.add(app, "forma2dRevolucion", ["A1", "A2", "A3", "A4"]).name('Forma de revolución');
+}
+	}).setValue('barrido');
+	// impresora.add(app, "forma2dRevolucion", ["A1", "A2", "A3", "A4"]).name('Forma de revolución');
+	// impresora.add(app, "forma2dBarrido", ["B1", "B2", "B3", "B4"]).name('Forma de barrido');
 	impresora.open();
 
 	var instrucciones = gui.addFolder('Instrucciones');
