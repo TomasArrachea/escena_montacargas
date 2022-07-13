4f9fb31eaca0c4197215615a53ef41c3d3753973
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-13 17:19:53 -0300

    gui update angulo solo en barrido

--- js/gui.js
+++ js/gui.js
@@ -17,29 +17,31 @@ function initMenu(escena) {
 		}
 	};
 	var gui = new dat.GUI();
+
+	var instrucciones = gui.addFolder('Instrucciones');
+	instrucciones.add(app, "verComandos").name('Ver instrucciones');
+	
 	var impresora = gui.addFolder('Impresora');
-	var controllerBarrido, controllerRev;
+	var controllerBarrido, controllerRev, controllerAngulo;
 
 	impresora.add(app, "generarImpresion").name('GENERAR IMPRESIÓN');
 	impresora.add(app, "textura", ['patron1', 'patron2', 'patron3', 'patron4', 'patron5', 'patron6', 'patron7']).name('Textura');
-	impresora.add(app, "angulo", 0, 360).step(1).name('Ángulo de torsión');
 	impresora.add(app, "tipoSuperficie", ["barrido", "revolucion"]).name('Tipo de superficie').onFinishChange(function (value) {
 		if (value == 'barrido') {
 			if (controllerRev)
 				controllerRev.remove();
 			controllerBarrido = impresora.add(app, "forma2dBarrido", ["B1", "B2", "B3", "B4"]).name('Forma de barrido');
+			controllerAngulo = impresora.add(app, "angulo", 0, 360).step(1).name('Ángulo de torsión');
 
 		} else {
 			if (controllerBarrido)
 				controllerBarrido.remove();
+			if (controllerAngulo)
+				controllerAngulo.remove();
 			controllerRev = impresora.add(app, "forma2dRevolucion", ["A1", "A2", "A3", "A4"]).name('Forma de revolución');
-}
+		}
 	}).setValue('barrido');
 	impresora.open();
-
-	var instrucciones = gui.addFolder('Instrucciones');
-	instrucciones.add(app, "verComandos").name('Ver instrucciones');
-	instrucciones.open();
 
 	gui.width = 300;
 
