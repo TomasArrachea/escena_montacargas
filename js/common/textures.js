1b7287af61cfe94e2ba08e2a21d809cd7aa3486a
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-15 16:16:39 -0300

    fix texturas ruedas

--- js/common/textures.js
+++ js/common/textures.js
@@ -3,4 +3,4 @@ export const RUST = 'maps/texturaGrua.jpg';
 export const WALL = 'maps/CorrugatedMetalPanel02_1K_BaseColor.png';
 export const PRINTER = 'maps/greyRoom1_back.jpg';
 export const FLOOR = 'maps/StoneTilesFloor01_1K_BaseColor.png';
-export const WHEEL = 'maps/rueda.jpg';
+export const WHEEL = 'maps/rueda.png';
