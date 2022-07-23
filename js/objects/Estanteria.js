e95cb84658e02eed4238d9fa423d2ac76f2f8f19
tomasarrachea <tarrachea@fi.uba.ar>
2022-07-23 18:50:11 -0300

    fix dejar objetos en la estanteria

--- js/objects/Estanteria.js
+++ js/objects/Estanteria.js
@@ -3,10 +3,35 @@ import { Objeto3D } from './objeto3d.js';
 import { Cubo } from "./Cubo.js";
 import { RGB_BROWN, RGB_GREY } from '../common/colors.js';
 
+class Hueco {
+    constructor(posicion) {
+        this.posicion = posicion;
+        this.objeto = null;
+    }
+
+    getPosicion() {
+        return this.posicion;
+    }
+
+    estaLibre() {
+        return this.objeto == null;
+    }
+
+    ocupar(objeto) {
+        this.objeto = objeto;
+    }
+
+    vaciar() {
+        var obj = this.objeto;
+        this.objeto = null;
+        return obj;
+    }
+}
+
 export class Estanteria extends Objeto3D {
     constructor(padre) {
         super(padre);
-        this.DISTANCIA_DEPOSITAR = 1.5;
+        this.DISTANCIA_DEPOSITAR = 0.5;
 
         // objeto vacio
         this.xEstanteria = 2;
@@ -41,43 +66,45 @@ export class Estanteria extends Objeto3D {
                 columna.setColor(RGB_BROWN);
                 this.agregarHijo(columna);
             }
+
+        var pos;
+        this.huecos = [];
+        for (var i = -4; i < 4; i++) {
+            pos = [0, 2 + this.yEstanteria / 2, this.zEstanteria / 16 + i * this.zEstanteria / 8]; // posicion del cada casillero de la estanteria
+            this.huecos.push(new Hueco(pos));
+            this.huecos.push(new Hueco([pos[0], pos[1] + 2.5, pos[2]]));
+        }
     }
 
     intentarAgregarObjeto(objeto, posObjeto) {
         // estanteria tiene 8x2 lugares. Elegir el que este mas cerca (cumpliendo con la minima distancia p depositar)
         var distMin = Number.MAX_SAFE_INTEGER;
-        var posMin = [0, 0, 0];
-        var dist;
-        var pos;
-        var posEstante = vec3.create();
-        for (var i = 0; i < 2; i++) {
-            for (var j = 0; j < 8; j++) {
-                pos = [0, 2 + i * 2.5 + 0.1, -this.zEstanteria / 2 + this.zEstanteria * 1 / 16 + j * this.zEstanteria / 7];
-                vec3.add(posEstante, this.posicion, pos);
-                dist = vec3.distance(posEstante, posObjeto);
-                console.log('dist: ' + dist);
+        var huecoMasCercano, dist, posMundo;
 
+        this.huecos.forEach(hueco => {
+            if (hueco.estaLibre()) {
+                posMundo = this.getPosicionMundo(hueco.getPosicion());
+                dist = vec3.distance(posObjeto, posMundo);
                 if (dist <= this.DISTANCIA_DEPOSITAR && dist < distMin) {
                     distMin = dist;
-                    posMin = pos;
+                    huecoMasCercano = hueco;
                 }
             }
-        }
+        });
 
         if (distMin == Number.MAX_SAFE_INTEGER)
             return false;
 
-        console.log('distMin: ' + distMin);
-
-        this.agregarObjeto(objeto, posMin);
+        this.agregarObjeto(objeto, huecoMasCercano);
         return true;
     }
 
-    agregarObjeto(objeto, posicion) {
-        console.log('agregando objeto a ' + posicion);
+    agregarObjeto(objeto, hueco) {
         this.agregarHijo(objeto);
         objeto.setEscala(0.4, 0.4, 0.4); // deshago la escala del carro
         objeto.setEscala(2, 2, 2); // deshago la escala de la estanteria
+        var posicion = hueco.getPosicion();    
         objeto.setPosicion(posicion[0], posicion[1], posicion[2]);
+        hueco.ocupar(objeto);
     }
 }
